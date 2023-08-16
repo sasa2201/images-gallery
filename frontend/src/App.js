f3583f11ca5e440bf67db0ca38013571dbb6f508
Shahbaz Atawoo <shahbaz220197@gmail.com>
2023-08-16 20:47:35 +0400

    reset search input upon submit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
 
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 function App() {
   const [word, setWord] = useState('');
@@ -10,12 +11,19 @@ function App() {
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word);
+    fetch(`https://api.unsplash.com/photos/random?query=${word}&client_id=${UNSPLASH_KEY}`)
+     .then((res) => res.json())
+     .then((data) =>{
+      console.log(data);
+     })
+     .catch((err) => {
+      console.log(err);
+     })
+     setWord('');
   }
 
-  console.log(word);
-
   return (
-    <div className="App">
+    <div>
       <Header title="Images Gallery"/>
       <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}/>
     </div>
