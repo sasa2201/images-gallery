f40b6f859b4b90776bad67cecad4f275121b5a12
Shahbaz Atawoo <shahbaz220197@gmail.com>
2023-11-23 20:46:10 +0400

    added text logo remove prettier

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import { Navbar , Container} from 'react-bootstrap';
+import { Navbar, Container } from 'react-bootstrap';
+import { ReactComponent as Logo } from '../images/logo.svg';
 
-const navbarStyle ={
-    backgroundColor: 'lightblue'
+const navbarStyle = {
+  backgroundColor: '#eeeeee',
 };
 
-const Header = ({title}) =>{
-    return(
-        <Navbar style={navbarStyle}  data-bs-theme="light">
-            <Container>
-            <Navbar.Brand href="/">{title}</Navbar.Brand>
-            </Container>
-        </Navbar>
-)
+const Header = ({ title }) => {
+  return (
+    <Navbar style={navbarStyle} data-bs-theme="light">
+      <Container>
+        <Logo style={{ maxWidth: '12rem', maxHeight: '2rem' }} />
+      </Container>
+    </Navbar>
+  );
 };
-
 export default Header;
