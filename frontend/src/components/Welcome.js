ea39803e64db7e112c56fbb9b3a2d66f57902280
Shahbaz Atawoo <shahbaz220197@gmail.com>
2023-11-23 22:11:51 +0400

    welcome component

--- frontend/src/components/Welcome.js
+++ frontend/src/components/Welcome.js
@@ -3,16 +3,13 @@ import { Jumbotron, Button } from 'react-bootstrap';
 
 const Welcome = () => (
   <Jumbotron>
-    <h1>Images Gallery</h1>
-    <p>
-      This is simple application that retrieves photos using Unspash API. In
-      order to start enter any search term in the input field.
-    </p>
-    <p>
-      <Button variant="primary" href="https://unsplash.com" target="_blank">
-        Learn more
-      </Button>
-    </p>
+  <h1>Images Gallery</h1>
+  <p>
+    This is a simple application that retrieves photos using Unsplash API. In order to start, enter any search term in the input field.
+  </p>
+  <p>
+    <Button variant="primary" href="https://unsplash.com" target="_blank">Learn more</Button>
+  </p>
   </Jumbotron>
 );
 
